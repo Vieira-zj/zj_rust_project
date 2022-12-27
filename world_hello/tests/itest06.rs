690ee0fab404b88efe33057d590b42c72952f4c4
zhengjin <zivieira@163.com>
2022-12-27 17:06:53 +0800

    Add channel demo.

--- world_hello/tests/itest06.rs
+++ world_hello/tests/itest06.rs
@@ -7,7 +7,7 @@ use std::thread;
 use std::time::Duration;
 
 #[test]
-fn it_create_and_wait_thread() {
+fn it_create_and_wait_thread_01() {
     // main 线程一旦结束，则程序随之结束，同时各个子线程也将被强行终止
     let handle = thread::spawn(|| {
         for i in 1..5 {
@@ -21,6 +21,25 @@ fn it_create_and_wait_thread() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(100));
     }
+}
+
+#[test]
+fn it_create_and_wait_thread_02() {
+    // 非 main 线程结束后，各个子线程仍然会继续运行
+    let new_thread = thread::spawn(|| {
+        println!("thread A creates thread B");
+        thread::spawn(|| {
+            for i in 0..5 {
+                println!("thread B is running at {}", i);
+                thread::sleep(Duration::from_millis(200));
+            }
+        });
+        println!("thread A exit");
+    });
+
+    new_thread.join().unwrap();
+    thread::sleep(Duration::from_secs(3));
+    println!("main exit");
 }
 
 #[test]
@@ -53,7 +72,7 @@ fn it_barrier_for_threads() {
     for handle in handles {
         handle.join().unwrap();
     }
-    println!("test done");
+    println!("barrier test done");
 }
 
 #[test]
@@ -76,6 +95,7 @@ fn it_thread_local() {
     });
     t.join().unwrap();
 
+    // 尽管子线程中修改为了 3, 我们在这里依然拥有main线程中的局部值 2
     FOO.with(|f| {
         assert_eq!(*f.borrow(), 2);
     });
@@ -131,3 +151,105 @@ fn it_thread_sync_once() {
     handle2.join().unwrap();
     println!("{}", unsafe { VAL });
 }
+
+//
+// Channel: send and receive message
+//
+
+#[test]
+fn it_mpsc_send_and_rec_msg() {
+    use std::sync::mpsc;
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        tx.send(1).unwrap();
+    });
+
+    println!("receive {}", rx.recv().unwrap());
+}
+
+#[test]
+fn it_mpsc_send_and_try_rec_msg() {
+    use std::sync::mpsc;
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        tx.send(1).unwrap();
+    }); // 发送者 tx 被 drop
+
+    println!("receive {:?}", rx.try_recv());
+    thread::sleep(Duration::from_millis(100));
+    println!("receive {:?}", rx.try_recv());
+    thread::sleep(Duration::from_millis(100));
+    println!("receive {:?}", rx.try_recv());
+}
+
+#[test]
+fn it_mpsc_rec_msg_in_for() {
+    use std::sync::mpsc;
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(200));
+        }
+    });
+
+    // 'for rx' exit when tx is dropped
+    for rec in rx {
+        println!("received: {}", rec);
+    }
+    println!("main exit");
+}
+
+#[test]
+fn it_mpsc_multiple_send() {
+    use std::sync::mpsc;
+
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+
+    thread::spawn(move || {
+        tx.send(String::from("hi from raw tx")).unwrap();
+    });
+    thread::spawn(move || {
+        tx1.send(String::from("hi from clone tx")).unwrap();
+    });
+
+    for rec in rx {
+        println!("received: {}", rec);
+    }
+    println!("main exit");
+}
+
+#[test]
+fn it_mpsc_send_and_rec_enum() {
+    use std::sync::mpsc::{self, Receiver, Sender};
+
+    enum Fruit {
+        Apple(u8),
+        Orange(String),
+    }
+
+    let (tx, rx): (Sender<Fruit>, Receiver<Fruit>) = mpsc::channel();
+    tx.send(Fruit::Orange("sweet".to_string())).unwrap();
+    tx.send(Fruit::Apple(1)).unwrap();
+
+    for _ in 0..2 {
+        match rx.recv().unwrap() {
+            Fruit::Apple(count) => println!("received {} apples", count),
+            Fruit::Orange(flavor) => println!("received {} orange", flavor),
+        }
+    }
+}
+
+//
+// 锁、Condvar 和信号量
+//
