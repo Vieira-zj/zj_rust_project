925fa08a8c4153d24bf6c4863ef402aeaff8bf29
zhengjin <zivieira@163.com>
2023-07-15 23:57:34 +0800

    Add custom error demo.

---
+++ world_hello/tests/base_test.rs
@@ -0,0 +1,154 @@
+use std::io::{BufRead, Read};
+
+// trait
+
+#[test]
+fn it_copy_trait() {
+    #[derive(Debug, Clone, Copy)]
+    struct KeyId(u32);
+    let k = KeyId(42);
+    let copied = k; // value bitwise copied from k to copied
+    println!("src key: {:?}", k);
+    println!("copied key: {:?}", copied);
+}
+
+// custom err for error handle
+
+#[test]
+fn it_custom_string_error() {
+    #[derive(Debug)]
+    struct MyError(String);
+
+    impl std::fmt::Display for MyError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    impl std::error::Error for MyError {}
+
+    impl std::convert::From<String> for MyError {
+        fn from(content: String) -> Self {
+            Self(content)
+        }
+    }
+
+    fn read_file(fpath: &str) -> Result<String, MyError> {
+        let mut s = String::new();
+        let mut f =
+            std::fs::File::open(fpath).map_err(|err| format!("fail open file: {:?}", err))?;
+        // 需要引入 std::io::Read 特征
+        f.read_to_string(&mut s)
+            .map_err(|err| format!("fail read: {:?}", err))?;
+        Ok(s)
+    }
+
+    match read_file("/tmp/test/nonexist.txt") {
+        Ok(content) => {
+            println!("read file:\n{}", content);
+        }
+        Err(err) => {
+            println!("get error: {}", err);
+        }
+    }
+}
+
+#[test]
+fn it_custom_nested_error() {
+    #[derive(Debug)]
+    enum MyError {
+        Io(std::io::Error),
+        Utf8(std::string::FromUtf8Error),
+        General(String),
+    }
+
+    impl std::fmt::Display for MyError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                MyError::Io(e) => write!(f, "IO error: {}", e),
+                MyError::Utf8(e) => write!(f, "utf-8 error: {}", e),
+                MyError::General(s) => write!(f, "general error: {}", s),
+            }
+        }
+    }
+
+    impl std::error::Error for MyError {
+        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+            match self {
+                MyError::Io(e) => Some(e),
+                MyError::Utf8(e) => Some(e),
+                MyError::General(_) => None,
+            }
+        }
+    }
+
+    impl From<std::io::Error> for MyError {
+        fn from(e: std::io::Error) -> Self {
+            Self::Io(e)
+        }
+    }
+    impl From<std::string::FromUtf8Error> for MyError {
+        fn from(e: std::string::FromUtf8Error) -> Self {
+            Self::Utf8(e)
+        }
+    }
+
+    fn read_first_line(fpath: &str) -> Result<String, MyError> {
+        let f = std::fs::File::open(fpath)?;
+        let mut reader = std::io::BufReader::new(f);
+        let mut buf = vec![];
+        let len = reader.read_until(b'\n', &mut buf)?;
+        if len > 1024 {
+            return Err(MyError::General(format!("line too long: {}", len)));
+        }
+        let result = String::from_utf8(buf)?;
+        Ok(result)
+    }
+
+    let result = read_first_line("/tmp/test/nonexist.txt");
+    match result {
+        Ok(line) => println!("read first line:\n{}", line),
+        Err(err) => println!("fail read:\n{}", err),
+    }
+}
+
+#[test]
+fn it_custom_wrapped_error() {
+    /// custom wrapped error by trait object.
+    #[derive(Debug)]
+    enum MyWrappedError {
+        Wrapped(Box<dyn std::error::Error>),
+        General(String),
+    }
+
+    impl std::fmt::Display for MyWrappedError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                MyWrappedError::Wrapped(err) => write!(f, "inner error: {}", err),
+                MyWrappedError::General(s) => write!(f, "general error: {}", s),
+            }
+        }
+    }
+
+    fn read_first_line(fpath: &str) -> Result<String, MyWrappedError> {
+        let f = std::fs::File::open(fpath).map_err(|err| MyWrappedError::Wrapped(Box::new(err)))?;
+        let mut reader = std::io::BufReader::new(f);
+        let mut buf = vec![];
+        let len = reader
+            .read_until(b'\n', &mut buf)
+            .map_err(|err| MyWrappedError::Wrapped(Box::new(err)))?;
+        if len > 1024 {
+            return Err(MyWrappedError::General(format!("line to long: {}", len)));
+        }
+
+        let result =
+            String::from_utf8(buf).map_err(|err| MyWrappedError::Wrapped(Box::new(err)))?;
+        Ok(result)
+    }
+
+    let result = read_first_line("/tmp/test/rust.txt");
+    match result {
+        Ok(line) => println!("read first line:\n{}", line),
+        Err(err) => println!("fail read:\n{}", err),
+    }
+}
